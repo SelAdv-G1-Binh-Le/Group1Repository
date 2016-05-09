0655f33a96e9a9b14432e43cf2ab2b77cac493a4
G1-Diep-Duong <lewin.yue@gmail.com>
2016-05-09 22:18:29 +0700

    5-09-10-18

--- PageObjects/MainPage.cs
+++ PageObjects/MainPage.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Group1Project.Common;
 
 namespace Group1Project.PageObjects
 {
@@ -13,6 +15,7 @@ namespace Group1Project.PageObjects
         #region Locators
 
         static readonly By _lblWelcome = By.XPath("//a[@href='#Welcome']");
+        static readonly By _lblRepository = By.XPath("//a[@href='#Repository']");
         static readonly By _lnkLogout = By.XPath("//a[@href='logout.do']");
 
         #endregion
@@ -22,6 +25,12 @@ namespace Group1Project.PageObjects
         {
             get { return Constant.WebDriver.FindElement(_lblWelcome); }
         }
+
+        public IWebElement LblRepository
+        {
+            get { return Constant.WebDriver.FindElement(_lblRepository); }
+        }
+
 
         public IWebElement LnkLogout
         {
