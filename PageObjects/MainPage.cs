8e27a73c9de3741d2a43f3047477a9bd27033f3a
G1-Diep-Duong <lewin.yue@gmail.com>
2016-05-09 09:52:54 +0700

    5-09-9-52

---
+++ PageObjects/MainPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group1Project.PageObjects
+{
+    class MainPage : GeneralPage
+    {
+
+
+        #region Locators
+
+        static readonly By _lblWelcome = By.XPath("//a[@href='#Welcome']");
+        static readonly By _lnkLogout = By.XPath("//a[@href='logout.do']");
+
+        #endregion
+
+        #region Elements
+        public IWebElement LblWelcome
+        {
+            get { return Constant.WebDriver.FindElement(_lblWelcome); }
+        }
+
+        public IWebElement LnkLogout
+        {
+            get { return Constant.WebDriver.FindElement(_lnkLogout); }
+        }
+        #endregion
+
+        #region Methods
+
+        public LoginPage Logout()
+        {
+            this.LblWelcome.Click();
+            this.LnkLogout.Click();
+
+            return new LoginPage();
+        }
+
+
+        #endregion
+    }
+}
