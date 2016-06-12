b6bffdcb9c6443d00c953f85b809be9054d86533
G1-Diep-Duong <lewin.yue@gmail.com>
2016-06-12 19:00:17 +0700

    6121900 - Updated TCs oder

--- Common/Constant.cs
+++ Common/Constant.cs
@@ -18,7 +18,7 @@ namespace Group1Project.Common
         public const string DefaultUsername = "administrator";
         public const string DefaultPassword = "";
         public const string DefaultRepository = "SampleRepository";
-        public const int DefaultTimeout = 5;
+        public const int DefaultTimeout = 10;
         public const string DefaultLogFolder = @"D:\TADashBoard";
 
 
--- TestCases/Testbase.cs
+++ TestCases/Testbase.cs
@@ -19,12 +19,12 @@ namespace Group1Project.TestCases
     {
         public IWebDriver webDriver;
 
-        //DirectoryInfo di = Directory.CreateDirectory(Constant.DefaultLogFolder);
-        //Stopwatch stopWatch = new Stopwatch();
-        //FileStream ostrm;
-        //StreamWriter writer;
-        //TextWriter oldOut = Console.Out;
-        //string filename = "TA DashBoard-" + CommonMethods.ConvertDateTimeToString(DateTime.Now) + ".log";
+        DirectoryInfo di = Directory.CreateDirectory(Constant.DefaultLogFolder);
+        Stopwatch stopWatch = new Stopwatch();
+        FileStream ostrm;
+        StreamWriter writer;
+        TextWriter oldOut = Console.Out;
+        string filename = "TA DashBoard-" + CommonMethods.ConvertDateTimeToString(DateTime.Now) + ".log";
 
         /// <summary>
         /// Tests the initialize method.
@@ -34,21 +34,21 @@ namespace Group1Project.TestCases
         [TestInitialize]
         public void TestInitializeMethod()
         {
-            //try
-            //{
-            //    ostrm = new FileStream(Constant.DefaultLogFolder + "\\" + filename, FileMode.OpenOrCreate, FileAccess.Write);
-            //    writer = new StreamWriter(ostrm);
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine("- Cannot open " + Constant.DefaultLogFolder + " for writing");
-            //    Console.WriteLine(e.Message);
-            //    return;
-            //}
-            //Console.SetOut(writer);
+            try
+            {
+                ostrm = new FileStream(Constant.DefaultLogFolder + "\\" + filename, FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("- Cannot open " + Constant.DefaultLogFolder + " for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.SetOut(writer);
 
-            //Console.WriteLine("- Run Test Initialize");
-            //stopWatch.Start();
+            Console.WriteLine("- Run Test Initialize");
+            stopWatch.Start();
 
             // --- Old Settings ----------------------------------------------------------
             //Start Firefox browser and maximize window            
@@ -82,14 +82,14 @@ namespace Group1Project.TestCases
             Console.WriteLine("- Run Test Cleanup");
             // CLose browser
             webDriver.Quit();
-            //Console.WriteLine("TC finishes in {0} seconds", stopWatch.ElapsedMilliseconds / 1000);
-            //stopWatch.Stop();
+            Console.WriteLine("TC finishes in {0} seconds", stopWatch.ElapsedMilliseconds / 1000);
+            stopWatch.Stop();
 
-            ////Write log area
-            //Console.SetOut(oldOut);
-            //writer.Close();
-            //ostrm.Close();
-            //Console.WriteLine("- Please see the log file in " + Constant.DefaultLogFolder + "\\" + filename);
+            //Write log area
+            Console.SetOut(oldOut);
+            writer.Close();
+            ostrm.Close();
+            Console.WriteLine("- Please see the log file in " + Constant.DefaultLogFolder + "\\" + filename);
         }
     }
 }
